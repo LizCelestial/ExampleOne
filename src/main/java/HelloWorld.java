c3ae2313420a1a8c71f6bcfe0c684f94d92798cd
LizCelestial <celestelyse123@gmail.com>
2020-02-19 07:30:18 +0200

    Just Edited  on  19/Feb/20

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String [] args){
+
         System.out.println("Hello to the world..");
+
+        System.out.println("Hi , Welcome");
     }
 }
